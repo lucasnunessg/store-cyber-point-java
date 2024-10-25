1126a8b932ddb9211b0080765c6cd31f0b8b4c24
lucasnunessg <lucasnunespacheco@gmail.com>
2024-10-25 16:44:53 -0300

    começadno a tabela sales e dando pre authority para algumas rotas

--- src/main/java/com/cyberpoint/controller/ProductController.java
+++ src/main/java/com/cyberpoint/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ public class ProductController {
   }
 
   @PostMapping
+  @PreAuthorize("hasAuthority('ADMIN')")
   @ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<ProductDto> createProduct(@RequestBody ProductCreateDto productCreateDto) {
     Optional<Product> existingProduct = productService.findProductByName(productCreateDto.name());
