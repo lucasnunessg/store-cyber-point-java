8e91c3c77a4048d02f4fe89e0c5fec5bbf1ef10f
lucasnunessg <lucasnunespacheco@gmail.com>
2024-10-28 21:20:42 -0300

    decidi nao criar uma quarta tabela

--- src/main/java/com/cyberpoint/entity/Sales.java
+++ src/main/java/com/cyberpoint/entity/Sales.java
@@ -1,10 +1,40 @@
 package com.cyberpoint.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "sales")
 public class Sales {
 
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private LocalDateTime saleDate;
+
+  public Sales(Long id, LocalDateTime saleDate) {
+    this.id = id;
+    this.saleDate = LocalDateTime.now();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+
+  public LocalDateTime getSaleDate() {
+    return saleDate;
+  }
+
+  public void setSaleDate(LocalDateTime saleDate) {
+    this.saleDate = saleDate;
+  }
 }
