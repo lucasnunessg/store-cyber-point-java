1ece11ab9bfd2dc86a855c8b292119c5882f3f51
lucasnunessg <lucasnunespacheco@gmail.com>
2024-11-18 16:36:53 -0300

    mais funções de login

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,20 +1,32 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from 'jwt-decode';
 import axios from 'axios';
 
 interface DecodedToken {
-    sub: string;
-    role: string;
-  };
+  sub: string;
+  role: string;
+}
 
 function Login() {
   const [username, setUsername] = useState(""); 
   const [password, setPassword] = useState<string>(""); 
   const [error, setError] = useState<string | null>(null);
+  const [welcomeUsernameFromToken, setWelcomeUsernameFromToken] = useState<string | null>(null);
   const navigate = useNavigate();
 
+  const handleLogout = () => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');  // token também
+    setWelcomeUsernameFromToken(null); // Atualiza o estado para remover a mensagem de boas-vindas
+    window.location.reload();
+    navigate('/'); 
+  };
 
+  useEffect(() => {
+    const storedUserName = localStorage.getItem('username');
+    setWelcomeUsernameFromToken(storedUserName);
+  })
 
   const handleUsername = (event: ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
@@ -27,28 +39,30 @@ function Login() {
   const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setError(null);
-    axios.post("http://localhost:8080/auth/login", {
+    try {
+      const response = await axios.post("http://localhost:8080/auth/login", {
         username: username, 
         password: password
-      }).then((response) => {
-        const token = response.data.token;
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-        localStorage.setItem('token', token);
-        const decodedToken = jwtDecode<DecodedToken>(token);
-        const userName = decodedToken.sub;
-        setUsername(userName);
-        localStorage.setItem('username', userName);
-       navigate('/products');
-      window.location.reload();        
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+      });
 
-        setError(error.response?.data.message || 'An error occurred')
-      });    
-     
-          
-  }
+      const token = response.data.token;
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      localStorage.setItem('token', token);
+
+      const decodedToken = jwtDecode<DecodedToken>(token);
+      const userName = decodedToken.sub;
+
+      setWelcomeUsernameFromToken(userName); // Exibe mensagem de boas-vindas
+      localStorage.setItem('username', userName);
+      
+      navigate('/products');
+      window.location.reload();
+    } catch (error: any) {
+      console.error('Error:', error);
+      setError(error.response?.data.message || 'An error occurred');
+      setWelcomeUsernameFromToken(null);
+    }
+  };
 
   return (
     <div>
@@ -73,8 +87,13 @@ function Login() {
             required
           />
         </div>
+        {welcomeUsernameFromToken && (
+          <h3>Seja bem-vindo, {welcomeUsernameFromToken}!</h3>
+        )}
         {error && <p style={{ color: 'red' }}>{error}</p>}
         <button type="submit">Login</button> 
+        <button type="button" onClick={handleLogout}>Logout</button> {/* Botão de logout */}
+ 
       </form>
     </div>
   );
